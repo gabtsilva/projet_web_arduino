7efbc351a8ac6a161e43864b1b053f490c4e6347
john doe <gabriel.torres.silva@hotmail.com>
2023-11-17 14:10:16 +0100

    Changed ESP AP to WiFi Connexion

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,10 +2,11 @@
 #include <WebSocketsServer.h>
 #include <DHT.h>
 
-const char *ssid = "ESP-8266-GA";    // Set the SSID (name) of the access point
+const char *ssid = "not_eduroam";    // Set the SSID (name) of the access point
 const char *password = "12345678";  // Set the password of the access point
 
 const int DHTPin = 0;  // Pin connected to the DHT11 sensor
+const int FANPin = 4;
 DHT dht(DHTPin, DHT11);
 
 WebSocketsServer webSocket = WebSocketsServer(80);
@@ -20,12 +21,20 @@ void handleWebSocketMessage(uint8_t num, WStype_t type, uint8_t *payload, size_t
 
 void setup() {
   Serial.begin(115200);
+  pinMode(FANPin, OUTPUT);
 
   dht.begin();
-  WiFi.softAP(ssid, password);
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
 
-  Serial.println("Access Point IP address: " + WiFi.softAPIP().toString());
-
+  // Print the IP address of the ESP8266
+  Serial.println("IP address: " + WiFi.localIP().toString());
+  
   webSocket.begin();
   webSocket.onEvent(handleWebSocketMessage);
 }
@@ -35,6 +44,12 @@ void loop() {
 
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
+  digitalWrite(FANPin,HIGH);
+  // if(temperature >= 25.00){
+  //   digitalWrite(FANPin, HIGH);
+  // }else{
+  //   digitalWrite(FANPin, LOW);
+  // }
 
   String jsonData = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
   Serial.println(humidity);
